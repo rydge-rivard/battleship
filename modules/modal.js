cc482d4f7e6d60cbcb39ef6227e54dd510b81290
Rydge <96926831+rydge-rivard@users.noreply.github.com>
2023-11-23 09:08:22 +0100

    Add fields in modal to get ship coordinates.

--- modules/modal.js
+++ modules/modal.js
@@ -3,10 +3,7 @@ export { modModal };
 const modModal = (function () {
   const dialog = document.querySelector("dialog");
   const confirmBtn = dialog.querySelector("#confirm");
-  const cancelBtn = dialog.querySelector("#cancel");
   const modalCont = dialog.querySelector(".modal-board");
-
-  cancelBtn.addEventListener("click", () => dialog.close());
 
   function showModal() {
     dialog.showModal();
